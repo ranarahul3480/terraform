9e92c4aac05106667ace389d8084f97e4daa3c21
ranarahul3480 <rana.rahul3480@gmail.com>
2024-02-10 13:31:40 +0530

    adding ec2 code

---
+++ aws-ec2/variables.tf
@@ -0,0 +1,12 @@
+variable "instance_type" {
+  description = "The type of instance to start"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Key name of the Key Pair to use for the instance; which can be managed using the `aws_key_pair` resource"
+  type        = string
+  default     = null
+}
+
